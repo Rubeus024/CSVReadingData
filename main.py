e4c57c79529c48bf19d3bc7500d4c4052858a018
denis007 <denis007@poczta.onet.eu>
2017-12-07 10:52:04 +0100

    Test2

--- main.py
+++ main.py
@@ -3,28 +3,29 @@ import matplotlib.pyplot as plt
 import calendar
 
 with open('csvFile.csv') as myFile:
-    daySumEnchanced=[0.0]*31
+    sumOfDays= [0.0] * 31
     read = csv.DictReader(myFile, delimiter=';')
-    sumTotal=0
+    sumTotal=0.0
+    averageExpenses=0.0
+
+    #Wydobywanie informacji z pliku csv
     for row in read:
-        # print(row.__class__)
         if (row['Type'] == '1'):
             day, month, year = row['Operation date'].split('-')
-
+            #Liczenie wydatków przypadający na kolejne dni
             if(day[0]=='0'):
                 day=day[1]
-            daySumEnchanced[int(day)-1]+=float(row["Amount"])
+            sumOfDays[int(day) - 1]+=float(row["Amount"])
 
 
-            #print(day + "  miesiac: " + month + "   rok: " + year)
-
-sum=sum(daySumEnchanced)
-print(sum)
+#Wyniki
+sumTotal=sum(sumOfDays)
+averageExpenses=sumTotal/len(sumOfDays)
+print("Suma: "+str(sumTotal))
+print("Średnia: "+str(averageExpenses))
 names=list(range(1,32)) # do obadania jutro
-plt.bar(names,daySumEnchanced)
+plt.bar(names, sumOfDays)
 plt.ylabel('Rampapam')
-#plt.show()
-print("Changes!")
-print("blabla")
-print("blablah")
+plt.show()
+
 
