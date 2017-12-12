296f410bd6884f198f28040a69d3a8c9981342e3
denis007 <denis007@poczta.onet.eu>
2017-12-12 20:50:33 +0100

    Program został w miarę uporządkowany -Trzeba dopracować ilość dni w poszczególnych miesiącach -Dodać średni wydatek oraz sumę wydatków na wykresach

--- main.py
+++ main.py
@@ -1,65 +1,69 @@
 import csv
+from matplotlib.backends.backend_pdf import PdfPages
 import matplotlib.pyplot as plt
-import calendar
-
-sumOfDays= [0.0] * 31
-sumTotal=0.0
-averageExpenses=0.0
-months=list(range(1,13))
-
-listOfMonths=[]
 
 
 class ChartData:
     def __init__(self):
-        self.sumOfDays = [0.0] * 31
+        self.sumOfDays = [0.0] * 31 # do poprawy - nie każdy miesiąc ma 31 dni!!!!!!!!!!!!
         self.sumTotal = 0.0
         self.averageExpenses = 0.0
-        self.months = list(range(1, 13))
 
     def DayConverter(self,dayCopy,row):
+        #kasowanie pierwszej cyferki (format [00])
         if (dayCopy[0] == '0'):
-            dayCopy = dayCopy[1]  # nie rozumiem
+            dayCopy = dayCopy[1]
+
         self.sumOfDays[int(dayCopy) - 1] += float(row["Amount"])
 
-for i in list(range(1,13)):
-    listOfMonths.append(ChartData() )
-    print(str(i))
+    def SumTotal(self):
+        self.sumTotal=sum(self.sumOfDays)
 
+    def AverageExpenses(self):
+        self.averageExpenses=self.sumTotal/len(self.sumOfDays)
 
-def DayConverter(dayCopy):
-    if (dayCopy[0] == '0'):
-        dayCopy = dayCopy[1] # nie rozumiem
-    sumOfDays[int(day) - 1] += float(row["Amount"])
+#Obiekt zapisujący wykresy
+pp = PdfPages("charts.pdf")
+#Lista miesięcy
+listOfMonths=[ChartData()]*13
 
-
+#Ciało programu
 with open('csvFile.csv') as myFile:
     read = csv.DictReader(myFile, delimiter=';')
-    print(months)
 
     #Wydobywanie informacji z pliku csv
     for row in read:
-        if (row['Type'] == '1'):
-            day, month, year = row['Operation date'].split('-')
-            if(str(month)=='11'):
-                print(str(day),str(month))
-                #Liczenie wydatków przypadający na kolejne dni
-                DayConverter(day)
-            elif(str(month)=='12'):
-                print(str(day), str(month))
-                DayConverter(day)
+        day, month, year = row['Operation date'].split('-')
+
+        #Na razie tylko dwa miesiące
+        if(str(month)=='11'):
+            #Liczenie wydatków przypadający na kolejne dni
+            listOfMonths[11].DayConverter(str(day),row)
+        elif(str(month)=='12'):
+            listOfMonths[12].DayConverter(str(day), row)
 
 
+#generowanie średniej oraz sumy
+for i in listOfMonths:
+    i.SumTotal()
+    i.AverageExpenses()
 
-#Wyniki
-sumTotal=sum(sumOfDays)
-averageExpenses=sumTotal/len(sumOfDays)
-print("Suma: "+str(sumTotal))
-print("Średnia: "+str(averageExpenses))
-names=list(range(1,32)) # do obadania jutro
-plt.bar(names, sumOfDays)
+#Prezentacja wyników
+plt.subplot()
+plt.bar(range(1,32), listOfMonths[11].sumOfDays)
 plt.ylabel('Rampapam')
+plt.title("Listopad")
+pp.savefig()
 #plt.show()
+plt.bar(range(1,32), listOfMonths[12].sumOfDays)
+plt.title("Grudzień")
+pp.savefig()
+#plt.show()
+
+pp.close()
+
+
+
 
 
 
