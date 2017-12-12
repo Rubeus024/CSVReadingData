414910e5a995c49803025f3d6ae16372b1335000
denis007 <denis007@poczta.onet.eu>
2017-12-12 13:58:36 +0100

    Update

--- main.py
+++ main.py
@@ -2,20 +2,34 @@ import csv
 import matplotlib.pyplot as plt
 import calendar
 
+sumOfDays= [0.0] * 31
+sumTotal=0.0
+averageExpenses=0.0
+months=list(range(1,13))
+
+
+def DayConverter(dayCopy):
+    if (dayCopy[0] == '0'):
+        dayCopy = dayCopy[1] # nie rozumiem
+    sumOfDays[int(day) - 1] += float(row["Amount"])
+
+
 with open('csvFile.csv') as myFile:
-    sumOfDays= [0.0] * 31
     read = csv.DictReader(myFile, delimiter=';')
-    sumTotal=0.0
-    averageExpenses=0.0
+    print(months)
 
     #Wydobywanie informacji z pliku csv
     for row in read:
         if (row['Type'] == '1'):
             day, month, year = row['Operation date'].split('-')
-            #Liczenie wydatków przypadający na kolejne dni
-            if(day[0]=='0'):
-                day=day[1]
-            sumOfDays[int(day) - 1]+=float(row["Amount"])
+            if(str(month)=='11'):
+                print(str(day),str(month))
+                #Liczenie wydatków przypadający na kolejne dni
+                DayConverter(day)
+            elif(str(month)=='12'):
+                print(str(day), str(month))
+                DayConverter(day)
+
 
 
 #Wyniki
@@ -26,6 +40,8 @@ print("Średnia: "+str(averageExpenses))
 names=list(range(1,32)) # do obadania jutro
 plt.bar(names, sumOfDays)
 plt.ylabel('Rampapam')
-plt.show()
+#plt.show()
+
+
 
 
