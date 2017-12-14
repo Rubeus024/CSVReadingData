b376a482f6c51af8b6c7fcc02b0abcddb92a040c
denis007 <denis007@poczta.onet.eu>
2017-12-14 10:28:29 +0100

    -dodano rysowanie wykresu z podziałem na kategorie
    -Jest różnica między listOfMonths=[ChartData()]*12
    a pętlą for in z inicjalizacją obiektów!

--- main.py
+++ main.py
@@ -3,15 +3,35 @@ from matplotlib.backends.backend_pdf import PdfPages
 import matplotlib.pyplot as plt
 
 
-class CategoryData:
+class OutputPictures:
 
-    def __init__(self):
-        self.categoryAmount=0.0;
+    @staticmethod
+    def write(month):
 
-    def add(self,categoryamount):
-        self.categoryAmount+=categoryamount
+        if month.sumTotal != 0:
+            plt.bar(range(1, month.numberOfDays + 1), month.sumOfDays)
+            plt.ylabel('zł')
+            plt.title(month.monthString + ' - dzienne wydatki')
+            plt.gcf().text(0.2, 0.03, 'Sum:' + str(round(month.sumTotal, 2)) + ' zł,' + '  Average expenses: ' +
+                           str(round(month.averageExpenses, 2)) + ' zł', fontsize=12)
+            pp.savefig()
+            # opcjonalne pokazanie okienka z rysunkiem
+            # plt.show()
+            plt.close()
+
+            plt.bar(month.categoryList.keys(), month.categoryList.values())
+            plt.ylabel('zł')
+            plt.title('{} - podział na kategorie'.format(month.monthString))
+            pp.savefig()
+            # opcjonalne pokazanie okienka z rysunkiem
+            # plt.show()
+            plt.close()
+
 
 class ChartData:
+    '''
+    Przed uruchomieniem programu należy usunąć w pliku cvs zdublowaną kolumnę
+    '''
     def __init__(self, monthstring='', numberofdays=30):
         self.categoryList = {'Jedzenie': 0.0, 'Czynsz': 0.0, 'Transport': 0.0, 'Odzież': 0.0,
                              'Buty': 0.0, 'Zabawa': 0.0}
@@ -22,7 +42,8 @@ class ChartData:
         self.averageExpenses = 0.0
 
     def day_converter(self, daycopy, rowcopy):
-        if daycopy[0] == '0':          # kasowanie pierwszej cyferki (format [00])
+        # kasowanie pierwszej cyfry, o ile wystąpi format [00]
+        if daycopy[0] == '0':
             daycopy = daycopy[1]
 
         self.sumOfDays[int(daycopy) - 1] += float(rowcopy["Amount"])
@@ -31,10 +52,11 @@ class ChartData:
     def sum_total(self):
         self.sumTotal = sum(self.sumOfDays)
 
+    @staticmethod
     def average_expenses(self):  # do przerobienia
         self.averageExpenses = self.sumTotal / self.numberOfDays
 
-    def add(self,categoryamount, string):
+    def add(self, categoryamount, string):
         self.categoryList[string] += categoryamount
 
 # Obiekt zapisujący wykresy
@@ -48,11 +70,9 @@ listOfMonths = {'00': ChartData('Styczeń', 31), '01': ChartData('Styczeń', 31)
                 '08': ChartData('Sierpień', 31), '09': ChartData('Wrzesień', 30), '10': ChartData('Październik', 31),
                 '11': ChartData('Listopad', 30), '12': ChartData('Grudzień', 31)}
 
-#for obj in range(1, 14):
-#    listOfMonths.append(ChartData())
-
-
 # Ciało programu
+# Miesiące w listOfMonths są przyporządkowane do liczb typu string, w celu łatwego połączenia je
+# ze stringiem month z pliku csv
 with open('csvFile.csv') as myFile:
     read = csv.DictReader(myFile, delimiter=';')
 
@@ -60,38 +80,14 @@ with open('csvFile.csv') as myFile:
     for row in read:
         if row['Type'] == '1':
             day, month, year = row['Operation date'].split('-')
-            #print(row['Category name'])
-            # Na razie tylko dwa miesiące
 
-                # Liczenie wydatków przypadający na kolejne dni
+            # Liczenie wydatków przypadający na kolejne dni
             listOfMonths[month].day_converter(str(day), row)
             if row['Category'] in listOfMonths[month].categoryList:
                 listOfMonths[month].add(float(row['Amount']), row['Category'])
 
 
-
 # Generowanie średniej, sumy oraz zapisywanie wyników
-counter = 0 # można to poprawić?
 for key,value in listOfMonths.items():
-    for key1,value1 in value.categoryList.items():
-        print(key,value1)
-    value.sum_total()
-    value.average_expenses()
-    if value.sumTotal != 0:
-        plt.bar(range(1, value.numberOfDays+1), value.sumOfDays)
-        plt.ylabel('zł')
-        plt.title(value.monthString+' - dzienne wydatki')
-        plt.gcf().text(0.2, 0.03, 'Sum:' + str(round(value.sumTotal, 2)) + ' zł,' + '  Average expenses: ' +
-                       str(round(value.averageExpenses, 2)) + ' zł', fontsize=12)
-        pp.savefig()
-        plt.show()
-        plt.close()
-
-        plt.bar(value.categoryList.keys(), value.categoryList.values())
-        plt.ylabel('zł')
-        plt.title('{} - podział na kategorie'.format(value.monthString))
-        pp.savefig()
-        plt.show()
-    counter += 1
-
+    OutputPictures.write(value)
 pp.close()
