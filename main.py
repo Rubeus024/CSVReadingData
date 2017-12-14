9fc700347ef5982c21b349db25c757fcfa67f25d
denis007 <denis007@poczta.onet.eu>
2017-12-14 10:01:07 +0100

    -dodano rysowanie wykresu z podziałem na kategorie
    -Jest różnica między listOfMonths=[ChartData()]*12
    a pętlą for in z inicjalizacją obiektów!

--- main.py
+++ main.py
@@ -3,8 +3,18 @@ from matplotlib.backends.backend_pdf import PdfPages
 import matplotlib.pyplot as plt
 
 
+class CategoryData:
+
+    def __init__(self):
+        self.categoryAmount=0.0;
+
+    def add(self,categoryamount):
+        self.categoryAmount+=categoryamount
+
 class ChartData:
     def __init__(self, monthstring='', numberofdays=30):
+        self.categoryList = {'Jedzenie': 0.0, 'Czynsz': 0.0, 'Transport': 0.0, 'Odzież': 0.0,
+                             'Buty': 0.0, 'Zabawa': 0.0}
         self.numberOfDays = numberofdays
         self.monthString = monthstring
         self.sumOfDays = [0.0] * self.numberOfDays
@@ -24,6 +34,9 @@ class ChartData:
     def average_expenses(self):  # do przerobienia
         self.averageExpenses = self.sumTotal / self.numberOfDays
 
+    def add(self,categoryamount, string):
+        self.categoryList[string] += categoryamount
+
 # Obiekt zapisujący wykresy
 pp = PdfPages("charts.pdf")
 
@@ -34,6 +47,7 @@ listOfMonths = {'00': ChartData('Styczeń', 31), '01': ChartData('Styczeń', 31)
                 '06': ChartData('Czerwiec', 30), '07': ChartData('Lipiec', 31),
                 '08': ChartData('Sierpień', 31), '09': ChartData('Wrzesień', 30), '10': ChartData('Październik', 31),
                 '11': ChartData('Listopad', 30), '12': ChartData('Grudzień', 31)}
+
 #for obj in range(1, 14):
 #    listOfMonths.append(ChartData())
 
@@ -46,24 +60,36 @@ with open('csvFile.csv') as myFile:
     for row in read:
         if row['Type'] == '1':
             day, month, year = row['Operation date'].split('-')
-
+            #print(row['Category name'])
             # Na razie tylko dwa miesiące
 
                 # Liczenie wydatków przypadający na kolejne dni
             listOfMonths[month].day_converter(str(day), row)
+            if row['Category'] in listOfMonths[month].categoryList:
+                listOfMonths[month].add(float(row['Amount']), row['Category'])
+
 
 
 # Generowanie średniej, sumy oraz zapisywanie wyników
 counter = 0 # można to poprawić?
 for key,value in listOfMonths.items():
+    for key1,value1 in value.categoryList.items():
+        print(key,value1)
     value.sum_total()
     value.average_expenses()
     if value.sumTotal != 0:
-        plt.bar(range(1, value.numberOfDays+1), listOfMonths[str(counter)].sumOfDays)
+        plt.bar(range(1, value.numberOfDays+1), value.sumOfDays)
         plt.ylabel('zł')
-        plt.title(value.monthString)
+        plt.title(value.monthString+' - dzienne wydatki')
         plt.gcf().text(0.2, 0.03, 'Sum:' + str(round(value.sumTotal, 2)) + ' zł,' + '  Average expenses: ' +
                        str(round(value.averageExpenses, 2)) + ' zł', fontsize=12)
+        pp.savefig()
+        plt.show()
+        plt.close()
+
+        plt.bar(value.categoryList.keys(), value.categoryList.values())
+        plt.ylabel('zł')
+        plt.title('{} - podział na kategorie'.format(value.monthString))
         pp.savefig()
         plt.show()
     counter += 1
