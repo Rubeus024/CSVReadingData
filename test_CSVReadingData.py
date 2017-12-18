e3d5c93a571d9583279da4331128d7ca58ab4f17
Rubeus024 <wieja.d@gmail.com>
2017-12-18 08:45:10 +0100

    Create test_CSVReadingData.py

---
+++ test_CSVReadingData.py
@@ -0,0 +1,10 @@
+import unittest
+import from main import ChartData
+
+class TestCSVReadingData(unittest.TestCase):
+
+  def test_sum_total():
+    unittest.assertEqual(10,10)
+
+  if(__name__=='__main__'):
+    unittest.main()
